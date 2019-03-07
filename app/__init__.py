29fcf3465b1cbd708cc827d9ab3bbf527e3523cd
kakty3 <kakty3.mail@gmail.com>
2019-03-07 11:58:00 +0300

    add db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import uuid
 
 from flask import Flask, request, flash, redirect, url_for, send_from_directory, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 
 from .internet_archive import InternetArchive
@@ -11,6 +12,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config.from_pyfile('config.py', silent=True)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 ia_client = InternetArchive(
     access_key=app.config['IA_S3_ACCESS_KEY_ID'],
     secret_key=app.config['IA_S3_SECRET_ACCESS_KEY_ID'],
--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from . import db
 
 
@@ -7,6 +9,7 @@ class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String())
     url = db.Column(db.String(), unique=True)
+    created_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return '<Filename {}, url {}>'.format(self.filename, self.url)
