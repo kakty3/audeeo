081fbab741ce49ca3670685f7d828b70756a7549
kakty3 <kakty3.mail@gmail.com>
2019-03-08 19:49:50 +0300

    add 'pub_date' and 'file_size' fields to File model

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ class File(db.Model):
     filename = db.Column(db.String())
     url = db.Column(db.String(), unique=True)
     created_at = db.Column(db.DateTime(timezone=True), server_default=sql_now())
+    pub_date = db.Column(db.DateTime(timezone=True), server_default=sql_now())
+    file_size = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Filename {}, url {}>'.format(self.filename, self.url)
