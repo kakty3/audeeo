b5967281d945c0a789af0573fc68b997b335e8d8
kakty3 <kakty3.mail@gmail.com>
2019-03-10 15:12:49 +0300

    add default=0 to Episode.file_size

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.sql.functions import now as sql_now
+from sqlalchemy.sql.expression import text
 
 from . import db
 
@@ -11,7 +12,7 @@ class Episode(db.Model):
     url = db.Column(db.String(), unique=True)
     created_at = db.Column(db.DateTime(timezone=True), server_default=sql_now())
     pub_date = db.Column(db.DateTime(timezone=True), server_default=sql_now())
-    file_size = db.Column(db.Integer)
+    file_size = db.Column(db.Integer, server_default=text('0'))
 
     def __repr__(self):
         return '<Title {}, url {}>'.format(self.title, self.url)
