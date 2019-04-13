0a378353f8802d624fce7f1842715f568adfae8e
kakty3 <kakty3.mail@gmail.com>
2019-04-13 16:05:23 +0300

    visual refactoring

--- config.py
+++ config.py
@@ -7,11 +7,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    
     SECURITY_PASSWORD_SALT = os.getenv('FLASK_SECRET_KEY')
+    SECURITY_REGISTERABLE = True
+
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     INTERNET_ARCHIVE_IDENTIFIER = 'podcast-hosting-test-Ia8gi'
     IA_S3_ACCESS_KEY_ID = os.getenv('IA_S3_ACCESS_KEY_ID')
     IA_S3_SECRET_ACCESS_KEY_ID = os.getenv('IA_S3_SECRET_ACCESS_KEY_ID')
