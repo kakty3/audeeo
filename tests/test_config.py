97de4a0776c60f277bc386abc82b8456dd013021
kakty3 <kakty3.mail@gmail.com>
2019-09-10 11:44:52 +0300

    split tests to sepatate files

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,20 +1,13 @@
 import os
 
-import flask_security
 import pytest
 
-from audeeo import create_app, models
-from audeeo.database import db
+from audeeo import create_app
+
 
 @pytest.fixture
 def app():
-    app = create_app()
-    with app.app_context():
-        db.create_all()
-        yield app
-        # https://stackoverflow.com/questions/26350911/what-to-do-when-a-py-test-hangs-silently
-        db.session.remove()
-        db.drop_all()
+    return create_app()
 
 def test_development_config(app):
     app.config.from_object('config.DevelopmentConfig')
@@ -33,10 +26,3 @@ def test_production_config(app):
     assert not app.config['DEBUG']
     assert not app.config['TESTING']
     assert app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL')
-
-def test_ping(app):
-    client = app.test_client()
-    resp = client.get('/ping')
-    data = resp.data.decode()
-    assert resp.status_code == 200
-    assert data == 'pong'
