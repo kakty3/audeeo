7f8b4223fb0abee78178913b05beedc0b7600d1a
kakty3 <kakty3.mail@gmail.com>
2019-11-23 19:40:52 +0300

    Exit programm when required environment variable is not set

--- audeeo/settings.py
+++ audeeo/settings.py
@@ -1,20 +1,25 @@
-from environs import Env
+import sys
+
+from environs import Env, EnvError
 
 env = Env()
 
-ENV = env.str('FLASK_ENV', default='production')
-DEBUG = ENV == 'development'
-TESTING = env.bool('FLASK_TESTING', default=False)
+try:
+    ENV = env.str('FLASK_ENV', default='production')
+    DEBUG = ENV == 'development'
+    TESTING = env.bool('FLASK_TESTING', default=False)
 
-CSRF_ENABLED = True
-SECRET_KEY = env.str('FLASK_SECRET_KEY')
+    CSRF_ENABLED = True
+    SECRET_KEY = env.str('FLASK_SECRET_KEY')
 
-SECURITY_PASSWORD_SALT = SECRET_KEY
-SECURITY_REGISTERABLE = True
-SECURITY_SEND_REGISTER_EMAIL = False
+    SECURITY_PASSWORD_SALT = SECRET_KEY
+    SECURITY_REGISTERABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = False
 
-SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-IA_S3_ACCESS_KEY_ID = env.str('IA_S3_ACCESS_KEY_ID')
-IA_S3_SECRET_ACCESS_KEY_ID = env.str('IA_S3_SECRET_ACCESS_KEY_ID')
+    IA_S3_ACCESS_KEY_ID = env.str('IA_S3_ACCESS_KEY_ID')
+    IA_S3_SECRET_ACCESS_KEY_ID = env.str('IA_S3_SECRET_ACCESS_KEY_ID')
+except EnvError as e:
+    sys.exit('Error: {}'.format(e))
